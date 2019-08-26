4ba0b3f674ee2409cb70003737828a228bd2a5ef
Kirill Apanasiuk <Micapka@yandex.ru>
2019-08-26 15:34:47 +0300

    add header

--- src/index.js
+++ src/index.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Header extends React.Component {
+  renderHeaderRightElements(str) {
+    if (!str) {
+      return (
+        <a href="">
+          <img
+            className="search-btn"
+            src={require("./img/whitesearchicon.png")}
+            alt=""
+          />
+        </a>
+      );
+    } else {
+      let strName = str.toUpperCase();
+      return (
+        <a href="">
+          <HeaderRightElements name={strName} />
+        </a>
+      );
+    }
+  }
+  render() {
+    return (
+      <div className="header">
+        <div className="computer-header">
+          <div className="computer-header_left-part">
+            <div className="computer-header_left-part__logo-container">
+              <a href="">
+                <img
+                  className="computer-header_left-part__logo-container__img"
+                  src={require("./img/photoeditorsdk-export.png")}
+                  alt=""
+                />
+              </a>
+            </div>
+          </div>
+          <div className="computer-header_right-part">
+            {this.renderHeaderRightElements("projects")}
+            {this.renderHeaderRightElements("guides")}
+            {this.renderHeaderRightElements("blog")}
+            {this.renderHeaderRightElements("training & certification")}
+            {this.renderHeaderRightElements()}
+          </div>
+        </div>
+        <div className="mobile-header"></div>
+      </div>
+    );
+  }
+}
+class HeaderRightElements extends React.Component {
+  render() {
+    return <div className="nav">{this.props.name}</div>;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component {
+  render() {
+    return <div>Hello its main part</div>;
+  }
+}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<App />, document.getElementById("root"));
